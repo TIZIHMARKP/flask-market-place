3665647d452b2b083f1b88cefb0917e5af5b1d13
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-20 13:49:10 -0700

    Added the market_page url

--- market.py
+++ market.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello flaskMarket"
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
 
-@app.route('/about/<username>')
-def about_page(username):
-    return f"<h1> About Page of {username} <h1>"
+@app.route('/market')
+def market_page():
+    return render_template('market.html')
 
 
 
