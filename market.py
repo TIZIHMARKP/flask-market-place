dd9392b308a82abf934997aff12a9e3ff70167d2
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-31 14:36:13 +0100

    Success Initialising database market.db

--- market.py
+++ market.py
@@ -1,5 +1,17 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+
+# Import models after db initialization
+from models import Item
+
+
 
 @app.route('/')
 @app.route('/home')
@@ -18,5 +30,7 @@ def market_page():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=8082, debug=True)
 
