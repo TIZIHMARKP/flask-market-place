4ff734488ff4a4d7900e0cdf28eb4f3d1fe60e4c
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-09-02 11:33:38 +0100

    Created register forms

--- market/__init__.py
+++ market/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '0576c81c967f0ed39c84704f'
 db = SQLAlchemy(app)
 
 from market import routes
--- market/forms.py
+++ market/forms.py
@@ -2,13 +2,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
 
-class RegisterForm(FlaskForm):
-    
-    username = StringField(label='username')
-    email_address = StringField(label = 'email')
-    password1 = PasswordField(label = 'password1')
-    password2 = PasswordField(label = 'password2')
-    submit = SubmitField(label = 'submit')
+class RegisterForm(FlaskForm):     # It basically kind of gives a secondary title for our fields
+
+    username = StringField(label='User Name: ')
+    email_address = StringField(label = 'Email Address: ')
+    password1 = PasswordField(label = 'Password')
+    password2 = PasswordField(label = 'Confirm Password')
+    submit = SubmitField(label = 'Create Account')
 
 
 
