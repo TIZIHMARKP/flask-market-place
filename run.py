6142d92a23fb6a4bed4ef44a7f0a270eb3ae8d39
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-09-04 13:51:43 +0100

    success registering first user

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 
-from market import app
+from market import app, db
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        print("Tables created successfully")
     app.run(host='0.0.0.0', port=8082, debug=True)
 
-    
