470c95e1c28962b207a76e541dd1ffaf23329035
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-09-04 15:47:50 +0100

    Success hashing password

--- market/__init__.py
+++ market/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
@@ -7,5 +8,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '0576c81c967f0ed39c84704f'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
