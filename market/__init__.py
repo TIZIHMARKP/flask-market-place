670dcc4c21c478dd84c658a973a4e89c985d268b
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-09-05 04:40:44 +0100

    added login_manager

--- market/__init__.py
+++ market/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -9,5 +10,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '0576c81c967f0ed39c84704f'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
