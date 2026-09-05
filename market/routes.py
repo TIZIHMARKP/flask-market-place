3c7b6957fefc05d8686adf12ed7ddf8b7bad9432
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-09-05 05:36:56 +0100

    Implemented logout functionality

--- market/routes.py
+++ market/routes.py
@@ -4,7 +4,7 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
 
 
@@ -62,6 +62,15 @@ def login_page():
 
     return render_template('login.html', form = form)
 
+
+@app.route('/logout', methods = ['GET', 'POST'])
+def logout_page():
+
+    logout_user()
+
+    flash("You have been logged out", category='info')
+
+    return redirect(url_for('home_page'))
 
 
 
